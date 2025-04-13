8491052569aa28ee20f3b1933bab26572d107c6c
iuhapark <hanied04@gmail.com>
2025-04-14 04:41:10 +0900

    feat: repository, storage setting for messaging data

---
+++ spring/src/main/java/com/iuha/api/repository/ChatRoomRepository.java
@@ -0,0 +1,13 @@
+package com.iuha.api.repository;
+
+import com.iuha.api.entity.model.ChatMessage;
+import com.iuha.api.entity.model.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+}
