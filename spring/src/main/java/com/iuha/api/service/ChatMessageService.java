a74ef86f3e2cd433d9f687596e33c9e01a001bba
iuhapark <hanied04@gmail.com>
2025-04-14 04:41:58 +0900

    feat: implement method

---
+++ spring/src/main/java/com/iuha/api/service/ChatMessageService.java
@@ -0,0 +1,7 @@
+package com.iuha.api.service;
+
+import com.iuha.api.entity.model.ChatMessage;
+
+public interface ChatMessageService {
+    ChatMessage save(ChatMessage message);
+}
---
+++ spring/src/main/java/com/iuha/api/service/ChatMessageServiceImpl.java
@@ -0,0 +1,32 @@
+package com.iuha.api.service;
+
+import com.iuha.api.entity.model.ChatMessage;
+import com.iuha.api.repository.ChatMessageRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Service
+public class ChatMessageServiceImpl implements ChatMessageService {
+
+    private final ChatMessageRepository chatMessageRepository;
+
+    public ChatMessageServiceImpl(ChatMessageRepository chatMessageRepository) {
+        this.chatMessageRepository = chatMessageRepository;
+    }
+
+    public ChatMessage save(ChatMessage message) {
+        message = new ChatMessage(
+                message.getRoomId(),
+                message.getSender(),
+                message.getReceiver(),
+                message.getMessage(),
+                LocalDateTime.now()
+        );
+        return chatMessageRepository.save(message);
+    }
+
+
+}
