4f544ccb9636176962a1afa52228547a6c9456d0
iuhapark <hanied04@gmail.com>
2025-04-16 04:06:37 +0900

    Create Spring Boot App

---
+++ spring/src/main/java/com/iuha/com/IuhaApplication.java
@@ -0,0 +1,13 @@
+package com.iuha.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IuhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IuhaApplication.class, args);
+    }
+
+}
