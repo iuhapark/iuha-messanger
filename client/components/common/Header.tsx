3b34f7179dd2848d3486ff2cecaaebadae60da43
iuhapark <hanied04@gmail.com>
2025-04-16 12:25:52 +0900

    refactor: top-fixed Header

---
+++ client/components/common/Header.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Image from "next/image";
+import UserImage from "@/assets/img/user/sam.png";
+import ThemeToggle from "./ThemeToggle";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { logout } from "@/lib/auth";
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='logo'>iuha</div>
+      <div className='header-right'>
+        <ThemeToggle />
+        <button className='header-icon' onClick={logout}>
+          <LogoutIcon className='header-icon' />
+        </button>
+        <Image src={UserImage} alt='user' className='user-image' />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
