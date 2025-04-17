622bff77efb57d5282be6d3c0b491e9cd8a0d293
iuhapark <hanied04@gmail.com>
2025-04-17 14:25:55 +0900

    remove

--- client/utils/token.ts
+++
@@ -1,8 +0,0 @@
-import { jwtDecode } from 'jwt-decode'
-import { parseCookies } from 'nookies'
-
-export const userId = (): string => {
-  const token = parseCookies().accessToken
-  const decoded: any = jwtDecode(token)
-  return decoded.id
-}
