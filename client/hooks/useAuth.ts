0514fc00d653c180a5bb75f6609ba1907bb2c6b5
iuhapark <hanied04@gmail.com>
2025-04-19 14:49:10 +0900

    feat: add hooks

---
+++ client/hooks/useAuth.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import api from '@/lib/api';
+import { User } from '@/\btypes';
+
+export const useAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await api.get(`/auth/user`);
+        setUser(res.data);
+      } catch (err) {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return { user, loading };
+};
