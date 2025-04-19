1d3b2c8d3d8e38c0cfe5a0a1b435e0dbc9686531
iuhapark <hanied04@gmail.com>
2025-04-19 14:53:05 +0900

    feat: CORS, CSRF configurations

--- spring/src/main/java/com/iuha/api/config/SecurityConfig.java
+++ spring/src/main/java/com/iuha/api/config/SecurityConfig.java
@@ -4,11 +4,12 @@ import com.iuha.api.handler.OAuth2LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import com.iuha.api.entity.vo.Role;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,22 +21,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrfConfig) -> csrfConfig.disable())
-                .cors(Customizer.withDefaults())
-                .headers((headerConfig) -> headerConfig.frameOptions(
-                                frameOptionsConfig -> frameOptionsConfig.disable()))
-                .authorizeHttpRequests((authorizeRequest) -> authorizeRequest
-                        .requestMatchers("/api/chat/my", "/api/chat/all", "/api/chat/new", "/api/chat/save", "/api/chat/rooms").authenticated()
-
-                        .requestMatchers("/", "/css/**", "images/**", "/js/**", "/login/*", "/logout/*").permitAll()
+        http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(List.of("http://localhost:3000", "https://www.iuhapark.com"));
+                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    config.setAllowedHeaders(List.of("*"));
+                    config.setExposedHeaders(List.of("Authorization"));
+                    config.setAllowCredentials(true);
+                    return config;
+                }))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/chat/**", "/api/token/user").authenticated()
+                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/login/**", "/logout/**").permitAll()
                         .anyRequest().authenticated())
                 // 로그아웃 성공 시 / 주소로 이동
                 .logout((logoutConfig) -> logoutConfig.logoutSuccessUrl("http://localhost:3000/"))
                 // OAuth2 로그인 기능에 대한 여러 설정
-//                .oauth2Login(Customizer.withDefaults()); // 아래 코드와 동일한 결과
-                .oauth2Login((oauth) -> oauth.userInfoEndpoint((endpoint) -> endpoint.userService(customOAuth2UserService))
+                .oauth2Login((oauth) -> oauth
+                        .userInfoEndpoint((endpoint) -> endpoint.userService(customOAuth2UserService))
                         .successHandler(oAuth2LoginSuccessHandler));
-
         return http.build();
     }
 }
--- spring/src/main/java/com/iuha/api/config/WebConfig.java
+++ spring/src/main/java/com/iuha/api/config/WebConfig.java
@@ -1,19 +1,32 @@
 package com.iuha.api.config;
 
+import com.iuha.api.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // CORS를 적용할 URL 패턴
+        registry.addMapping("/api/**") // CORS를 적용할 URL 패턴
                 .allowedOrigins("http://localhost:3000", "https://www.iuhapark.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowedMethods("*")
+                .exposedHeaders("Authorization")
                 .allowCredentials(true);
 
     }
