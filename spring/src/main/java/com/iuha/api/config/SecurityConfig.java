4e36d519f96c8b9c8c6ad04c1b9f1806e54f2181
iuhapark <hanied04@gmail.com>
2025-04-20 15:46:44 +0900

    refactor: clean code

--- spring/src/main/java/com/iuha/api/config/SecurityConfig.java
+++ spring/src/main/java/com/iuha/api/config/SecurityConfig.java
@@ -20,35 +20,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(
-                        (csrfConfig) -> csrfConfig.disable()
-                )
-                .cors(
-                        Customizer.withDefaults()
-                )
-                .headers(
-                        (headerConfig) -> headerConfig.frameOptions(
-                                frameOptionsConfig -> frameOptionsConfig.disable()
-                        )
-                )
+        http.csrf((csrfConfig) -> csrfConfig.disable())
+                .cors(Customizer.withDefaults())
+                .headers((headerConfig) -> headerConfig.frameOptions(
+                                frameOptionsConfig -> frameOptionsConfig.disable()))
                 .authorizeHttpRequests((authorizeRequest) -> authorizeRequest
-                        .requestMatchers("/api/chat/**").authenticated()
+                        .requestMatchers("/api/chat/my", "/api/chat/all", "/api/chat/new", "/api/chat/save", "/api/chat/rooms").authenticated()
+
                         .requestMatchers("/", "/css/**", "images/**", "/js/**", "/login/*", "/logout/*").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .logout( // 로그아웃 성공 시 / 주소로 이동
-                        (logoutConfig) -> logoutConfig.logoutSuccessUrl("http://localhost:3000/")
-                )
+                        .anyRequest().authenticated())
+                // 로그아웃 성공 시 / 주소로 이동
+                .logout((logoutConfig) -> logoutConfig.logoutSuccessUrl("http://localhost:3000/"))
                 // OAuth2 로그인 기능에 대한 여러 설정
 //                .oauth2Login(Customizer.withDefaults()); // 아래 코드와 동일한 결과
-                .oauth2Login(
-                        (oauth) ->
-                            oauth.userInfoEndpoint(
-                                    (endpoint) -> endpoint.userService(customOAuth2UserService)
-                            )
-                                    .successHandler(oAuth2LoginSuccessHandler)
-                );
+                .oauth2Login((oauth) -> oauth.userInfoEndpoint((endpoint) -> endpoint.userService(customOAuth2UserService))
+                        .successHandler(oAuth2LoginSuccessHandler));
 
         return http.build();
     }
