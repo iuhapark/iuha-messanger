08cfe69c6169d47030c5e06ac134d9a79157c0cd
iuhapark <hanied04@gmail.com>
2025-04-23 14:11:19 +0900

    feat: adjust type

--- client/types.ts
+++ client/types.ts
@@ -1,17 +1,21 @@
-export interface ChatRoomProps {
-  roomId: string;
-}
-
-export interface Message {
-  id?: number;
-  roomId: string;
-  sender: string;
-  receiver: string;
-  message: string;
-  timestamp?: string;
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  name: string;
+  profile: string;
+  role: 'USER' | 'ADMIN';
 }
 
 export interface ChatRoom {
   id: string;
   name: string;
+}
+
+export interface Message {
+  id?: string;
+  senderId: string;
+  receiverId: string;
+  content: string;
+  timestamp?: string;
 }
