76d8f9d5bfa500c128bf52a24625a9070ba09a5f
iuhapark <hanied04@gmail.com>
2025-04-25 23:14:20 +0900

    refactor: change type definition

--- client/types.ts
+++ client/types.ts
@@ -1,3 +1,5 @@
+import { ChatStep } from "./config/type/Data";
+
 export interface User {
   id: string;
   username: string;
@@ -15,17 +17,18 @@ export interface ChatRoom {
 }
 
 export interface Message {
-  id?: string;
+  roomId: string;
   sender: string;
   receiver: string;
   message: string;
   timestamp?: string;
 }
 
-export interface ChatRoomProps {
-  roomId: string;
+export interface ChatRoomListProps {
+  onSelect: (room: ChatRoom) => void;
 }
 
-export interface ChatMessageProps {
-  roomId: string;
+export interface UserListProps {
+  setStep: (step: ChatStep) => void;
+  onSelect: (room: ChatRoom) => void;
 }
--- spring/src/main/java/com/iuha/api/entity/dto/ChatRoomDto.java
+++ spring/src/main/java/com/iuha/api/entity/dto/ChatRoomDto.java
@@ -1,13 +1,26 @@
 package com.iuha.api.entity.dto;
 
+import com.iuha.api.entity.model.ChatRoom;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ChatRoomDto {
     private String id;
     private String name;
-    private String opponentId;
-    private String opponentName;
+    private UserDto sender;
+    private UserDto receiver;
+
+    public ChatRoomDto(ChatRoom chatRoom) {
+        this.id = chatRoom.getId();
+        this.name = chatRoom.getName();
+        this.sender = new UserDto(chatRoom.getSender());
+        this.receiver = new UserDto(chatRoom.getReceiver());
+    }
+
 }
