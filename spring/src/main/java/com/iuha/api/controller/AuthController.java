379116b9bfc1f0d33a280cc0f46278a032579367
iuhapark <hanied04@gmail.com>
2025-04-26 23:15:26 +0900

    feat: add session validation in user endpoint

--- spring/src/main/java/com/iuha/api/controller/AuthController.java
+++ spring/src/main/java/com/iuha/api/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.iuha.api.config.auth.LoginUser;
 import com.iuha.api.entity.dto.SessionUser;
 import com.iuha.api.entity.dto.UserDto;
 import com.iuha.api.service.UserService;
+import com.iuha.api.util.exception.ExceptionUtil;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +23,10 @@ public class AuthController {
     private final UserService service;
 
     @GetMapping("/user")
-    public SessionUser user(@LoginUser SessionUser user) {
+    public SessionUser user(@LoginUser SessionUser user)  {
+        if (user == null) {
+            throw new ExceptionUtil.UnauthorizedException("세션이 만료되었습니다. 로그인이 필요합니다.");
+        }
         log.info("세션에서 꺼낸 유저: {}", user);
         return user;
     }
