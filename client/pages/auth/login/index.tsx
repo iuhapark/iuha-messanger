44f9893ae68567971402097f8ff1445711187cf9
iuhapark <hanied04@gmail.com>
2025-05-04 18:09:13 +0900

    refactor: clean up code

--- client/pages/auth/login/index.tsx
+++ client/pages/auth/login/index.tsx
@@ -4,12 +4,12 @@ import { login } from "@/lib/auth";
 
 const Login = () => {
   return (
-    <div className="login-page">
-      <form className="login-form" onSubmit={login}>
-        <h1 className="login-title">Login</h1>
-        <input type="text" name="username" placeholder="Username" required />
-        <input type="password" name="password" placeholder="Password" required />
-        <button type="submit">Login</button>
+    <div className='login-page'>
+      <form className='login-form' onSubmit={login}>
+        <h1 className='login-title'>Login</h1>
+        <input type='text' name='username' placeholder='Username' required />
+        <input type='password' name='password' placeholder='Password' required />
+        <button type='submit'>Login</button>
       </form>
       </div>
   );
