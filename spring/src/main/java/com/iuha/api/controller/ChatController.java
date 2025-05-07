b2e1510e17833337a2832364802edd458dc6cf48
iuhapark <hanied04@gmail.com>
2025-05-07 10:03:39 +0900

    refactor: update message handling and DTO

--- spring/src/main/java/com/iuha/api/controller/ChatController.java
+++ spring/src/main/java/com/iuha/api/controller/ChatController.java
@@ -1,8 +1,9 @@
 package com.iuha.api.controller;
 
-import com.iuha.api.entity.dto.ChatMessageDto;
-import com.iuha.api.entity.model.ChatMessage;
+import com.iuha.api.entity.dto.MessageDto;
+import com.iuha.api.entity.model.Message;
 import com.iuha.api.entity.model.ChatRoom;
+import com.iuha.api.entity.model.User;
 import com.iuha.api.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,8 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,17 +24,18 @@ public class ChatController {
     /* 메시지 전송 */
     @MessageMapping("/chat/{roomId}")
     @SendTo("/topic/{roomId}")
-    public ChatMessage sendMessage(ChatMessageDto dto) {
-        return chatMessageService.save(
-                new ChatMessage(
-                        null,
-                        ChatRoom.builder().id(dto.getRoomId()).build(),
-                        HtmlUtils.htmlEscape(dto.getSender()),
-                        HtmlUtils.htmlEscape(dto.getReceiver()),
-                        HtmlUtils.htmlEscape(dto.getMessage()),
-                        null
-                )
+    public MessageDto sendMessage(MessageDto dto) {
+        Message saved = chatMessageService.save(
+                Message.builder()
+                        .chatRoom(ChatRoom.builder().id(dto.getRoomId()).build())
+                        .sender(User.builder().id(dto.getSender().getId()).build())
+                        .type(Message.MessageType.TALK)
+                        .message(HtmlUtils.htmlEscape(dto.getMessage()))
+                        .timestamp(LocalDateTime.now())
+                        .build()
         );
+
+        return MessageDto.from(saved);
     }
 }
 
