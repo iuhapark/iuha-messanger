36df5c776f6a3d823c945189cc863cd0eb3d562e
iuhapark <hanied04@gmail.com>
2025-05-11 11:23:06 +0900

    refactor: rename

--- spring/src/main/java/com/iuha/api/util/ChatStorage.java
+++ spring/src/main/java/com/iuha/api/util/ChatStorage.java
@@ -1,30 +1,30 @@
 package com.iuha.api.util;
 
-import com.iuha.api.entity.model.ChatMessage;
+import com.iuha.api.entity.model.Message;
 import com.iuha.api.entity.model.ChatRoom;
 
 import java.util.*;
 
 public class ChatStorage {
-    private static final Map<String, List<ChatMessage>> chatMessages = new HashMap<>();
+    private static final Map<String, List<Message>> messages = new HashMap<>();
 
 
-    public static void saveMessage(ChatMessage message) {
+    public static void saveMessage(Message message) {
         /* 서버 저장 조건 */
 
         /* 그외 로컬 스토리지 저장 */
-        chatMessages.computeIfAbsent(message.getId(), k -> new ArrayList<>()).add(message);
+        messages.computeIfAbsent(message.getId(), k -> new ArrayList<>()).add(message);
     }
 
-    public static List<ChatMessage> getMessages(String roomId) {
-        return chatMessages.getOrDefault(roomId, new ArrayList<>());
+    public static List<Message> getMessages(String roomId) {
+        return messages.getOrDefault(roomId, new ArrayList<>());
     }
 
     public static Set<String> getAllRoomIds() {
-        return chatMessages.keySet();
+        return messages.keySet();
     }
 
-    public static ChatRoom createRoom(ChatRoom newRoom) {
-        return newRoom;
+    public static ChatRoom createRoom(ChatRoom newChatRoom) {
+        return newChatRoom;
     }
 }
