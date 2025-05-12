65596f031c4df501fea6c88e3ad3ee9dd92f317e
iuhapark <hanied04@gmail.com>
2025-05-12 11:25:45 +0900

    .

--- spring/src/main/java/com/iuha/api/repository/UserRepository.java
+++ spring/src/main/java/com/iuha/api/repository/UserRepository.java
@@ -1,13 +1,11 @@
 package com.iuha.api.repository;
 
-import com.iuha.api.entity.model.ChatRoom;
 import com.iuha.api.entity.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
