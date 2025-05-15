9ddc2a907461aefee21c37a2ef030f79907675cb
iuhapark <hanied04@gmail.com>
2025-05-15 15:47:51 +0900

    feat: wrap application in AuthProvider for authentication context

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,13 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Layout from "@/components/layout/Layout";
+import { AuthProvider } from "@/context/AuthContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>     
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
