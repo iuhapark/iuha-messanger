385a61c32b1dc6caa0cf0500b60a2045219d3110
iuhapark <hanied04@gmail.com>
2025-05-18 15:54:16 +0900

    feat: refactor home page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,112 +1,71 @@
-import { useAuth } from "@/hooks/useAuth";
-import { login } from "@/lib/auth";
-import { PAGE } from "@/lib/page";
-import { Geist, Geist_Mono } from "next/font/google";
+import { useAuth } from "@/context/AuthContext";
+import { notoSansKR } from "@/styles/fonts";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useState, useCallback } from "react";
+import { IoIosArrowRoundBack, IoLogoGithub, IoIosGlobe } from "react-icons/io";
+import { FaRegFileLines } from "react-icons/fa6";
+import Login from "./auth/login";
 import Chat from "./chat";
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export default function Home() {
   const { user, loading } = useAuth();
-  const router = useRouter();
-  
+  const [showLogin, setShowLogin] = useState(false);
+
+  const onBack = useCallback(() => setShowLogin(false), []);
+
   if (loading) return <div>Loading...</div>;
   if (user) return <Chat />;
 
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} content-wrapper grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
-        Login
-        <ol className='list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]'>
-          <li className='mb-2 tracking-[-.01em]'>
-            Get started by editing{' '}
-            <code className='bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold'>
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className='tracking-[-.01em]'>
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className='flex gap-4 items-center flex-col sm:flex-row'>
-          <button
-            className='rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto'
-            onClick={() => router.push(`${PAGE.LOGIN}`)}
-            role='button'
-            rel='noopener noreferrer'
-          >
-            Sign in
-          </button>
-          <button
-            className='rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto'
-            onClick={login}
-            role='button'
-            rel='noopener noreferrer'
-          >
-            Google
-          </button>
-        </div>
+    <div>
+      {showLogin && <IoIosArrowRoundBack onClick={onBack} className='icon' />}
+    <div className={`${notoSansKR.className} content-wrapper`}>
+      <main className='font-[family-name:var(--font-noto-sans-kr)]'>
+        {showLogin ? (
+            <Login onBack={onBack} />
+        ) : (
+          <>
+            <Image
+              src='/assets/img/main.png'
+              alt='iuha'
+              width={500}
+              height={500}
+              className='pt-8 px-4'
+            />
+            <video
+              className='video-background'
+              autoPlay
+              loop
+              muted
+              playsInline
+            >
+              <source src='gradient.mp4' type='video/mp4' />
+            </video>
+            <h1 className='text-[clamp(5rem,5vw,4rem)] font-semibold tracking-[-.01em] font-[family-name:var(--font-geist-sans)] text-black'>
+              iuha
+            </h1>
+            <button className='btn w-1/4' onClick={() => setShowLogin(true)}>
+              로그인
+            </button>
+            <p className='text-[clamp(1.5rem,2vw,2rem)] font-medium flex flex-col tracking-[-.01em] text-black'>
+              <span>실시간 채팅 앱.</span>
+              <span>언제든 모두와 대화하세요.</span>
+            </p>
+            <footer className='flex gap-4 mt-8 text-black'>
+              <a href='/' target='_blank' rel='noopener noreferrer'>
+                <FaRegFileLines /> Portfolio
+              </a>
+              <a href='https://github.com/iuhapark' target='_blank' rel='noopener noreferrer'>
+                <IoLogoGithub /> GitHub
+              </a>
+              <a href='https://iuhapark.github.io/' target='_blank' rel='noopener noreferrer'>
+                <IoIosGlobe /> Blog
+              </a>
+            </footer>
+          </>
+        )}
       </main>
-      <footer className='row-start-3 flex gap-[24px] flex-wrap items-center justify-center'>
-        <a
-          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-          href='/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            aria-hidden
-            src='/file.svg'
-            alt='File icon'
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-          href='https://github.com/iuhapark'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            aria-hidden
-            src='/github.svg'
-            alt='File icon'
-            width={16}
-            height={16}
-          />
-          GitHub
-        </a>
-        <a
-          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-          href='https://iuhapark.github.io/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            aria-hidden
-            src='/globe.svg'
-            alt='Globe icon'
-            width={16}
-            height={16}
-          />
-          Blog
-        </a>
-      </footer>
+    </div>
     </div>
   );
 }
