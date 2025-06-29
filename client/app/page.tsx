da7ecfb30d543ca378c2f1f0397ba56b7789b419
iuhapark <hanied04@gmail.com>
2025-06-30 04:23:06 +0900

    feat: home maintenance

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,13 +7,16 @@ import { Spacer } from "@heroui/react";
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center relative'>
-      <Landing />
       <div
         className='fixed inset-0 z-0 w-full h-full bg-no-repeat bg-cover bg-center opacity-70'
         style={{ backgroundImage: "url('/assets/img/home/main.jpeg')" }}
       />
-      <Feature />
-      <Showcase />
+      <div className='relative z-10 w-full flex flex-col items-center justify-center'>
+        <Landing />
+        <Feature />
+        <Spacer y={40} />
+        <Showcase />
+      </div>
       <Spacer y={40} />
     </div>
   );
